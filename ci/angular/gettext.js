df7e28178956c7f25da4d30557158ed291a3d877
Fabian Meul <Fabian.Meul@district01.be>
2016-01-15 12:23:14 +0100

    * Cleaned up gulpfile.js

---
+++ ci/angular/gettext.js
@@ -0,0 +1,51 @@
+// CI: Angular - Gettext.js
+// ---
+// Gettext implementation for translations
+
+var gulp = require("gulp");
+var angularGettext = require('angular-gettext');
+
+// Load the build Helper
+var buildHelper = require("./build-helpers.js");
+
+
+// Create pot file of all translations
+gulp.task("pot", function() {
+
+    // Globbing patterns
+    var patterns = [
+        config.env.dev.serve.dir + "app/**/*.{js|html}",
+        "!" + buildHelper.bowerConfig().directory
+    ];
+
+    // Angular gettext options
+    var options = {}
+
+    // Destination
+    var destination = config.env.dev.serve.dir + "/assets/translations/pot";
+
+    return gulp.src(patterns)
+        .pipe(angularGettext.extract("template.pot", options))
+        .pipe(gulp.dest(destination));
+});
+
+// Create json translations from the created PO files
+gulp.task("translations", function() {
+
+    // Globbing patterns
+    var patterns = [
+        config.env.dev.serve.dir + "assets/translations/po/**/*.po"
+    ];
+
+    // Angular gettext options
+    var options = {
+        format: "json"
+    };
+
+    // Destination
+    var destination = config.env.dev.serve.dir + "/assets/translations/json";
+
+    return gulp.src(patterns)
+        .pipe($.angularGettext.compile(options))
+        .pipe(gulp.dest(destination));
+});
--- ci/packages.js
+++ ci/packages.js
@@ -5,6 +5,7 @@
 var gulp = require("gulp");
 var webserver = require("gulp-webserver");
 var bower = require("gulp-bower");
+var wiredep = require("wiredep").stream;
 
 // Load the build configuration
 var config = require("./config/config.json");
@@ -14,3 +15,34 @@ gulp.task("bower", function() {
     // Return a stream to support async
     return bower();
 });
+
+// Bower integration
+gulp.task("wiredep", function () {
+
+    // Source
+    var source = config.env.dev.serve.dir = "index.html";
+
+    // Wiredep options
+    var options = {
+        directory: buildHelper.bowerConfig().directory,
+        fileTypes: {
+            html: {
+              block: /(([ \t]*)<!--\s*bower:*(\S*)\s*-->)(\n|\r|.)*?(<!--\s*endbower\s*-->)/gi,
+              detect: {
+                js: /<script.*src=[""]([^""]+)/gi,
+                css: /<link.*href=[""]([^""]+)/gi
+              },
+              replace: {
+                js: "<script src=\"{{filePath}}\"></script>",
+                css: "<link rel=\"stylesheet\" href=\"/{{filePath}}\" />"
+              }
+            },
+        }
+    };
+
+    var destination = config.env.dev.serve.dir;
+
+    return gulp.src(source)
+        .pipe(wiredep(options))
+        .pipe(gulp.dest(destination));
+    });
